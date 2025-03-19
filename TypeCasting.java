a1ed63d47b9242f230f0588ca634ce4b4788f880
SoronnadiNice <soronnadinice@gmail.com>
2025-03-19 15:57:52 +0100

    TypeCasting

---
+++ TypeCasting.java
@@ -0,0 +1,14 @@
+public class TypeCasting{
+	public static void main(String[] args){
+		//Widening typ casting
+		double weight = 678;
+		System.out.printf("Your weight is %f%n",weight);
+		
+		//Narrowing Type Casting ASCII Code for ? = 63
+		char alpha = '?';
+		
+		int asciiCode =(char) alpha;
+		System.out.printf("The ascii Code for ? is %d%n",asciiCode);
+		
+	}
+}
