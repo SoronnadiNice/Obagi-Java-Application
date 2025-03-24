d2e7478e45f99059fb80d610a247017f386f502b
SoronnadiNice <soronnadinice@gmail.com>
2025-03-24 15:59:27 +0100

    Operators\

---
+++ Chapter 2/DisplayNumbers.java
@@ -0,0 +1,21 @@
+public class DisplayNumbers{
+	public static void main(String[] args){
+		//Using one System.out.println
+		System.out.println("Using one system.out.println statment:");
+		System.out.println("1 2 3 4");
+		System.out.println();
+		  // (b) Using four System.out.print statements
+        System.out.println("Using four System.out.print statements:");
+        System.out.print("1 ");
+        System.out.print("2 ");
+        System.out.print("3 ");
+        System.out.print("4");
+        System.out.println(); 
+        System.out.println(); 
+
+        // (c) Using one System.out.printf statement
+        System.out.println("Using one System.out.printf statement:");
+        System.out.printf("%d %d %d %d%n", 1, 2, 3, 4);
+
+	}
+}
