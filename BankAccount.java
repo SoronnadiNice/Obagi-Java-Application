bc7d307b4af93c52ae3d51cf62fd84fe8e3764a2
SoronnadiNice <soronnadinice@gmail.com>
2025-04-07 10:30:20 +0100

    Chapter 4 assignmet

---
+++ BankAccount.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class BankAccount{
+	 String accountName = "John Edward";
+	 String accountNumber ="4590468934";
+	 double accountBalance = 94746.66;
+	 
+	 public static void main(String[] args){
+		 BankAccount bank = new BankAccount();
+		 
+		 System.out.println("Welcome to Opay");
+		 System.out.println("Enter 1 to withdraw");
+		 System.out.println("Enter 2 to deposit");
+		 
+		 System.out.print("Enter your choice");
+		 int choice =input.nextInt();
+		 
+		 switch(choice){
+			 case 1:{
+				  System.out.printf("Account name is %s%n",bank.accountName);
+		 
+		          System.out.printf("Account Number is %s%n",bank.accountNumber);
+		 
+		 
+		          System.out.printf("Account Balance is %.2f%n",bank.accountBalance);
+				  
+				  bank.withdrawal(amount);
+				  System.out.printf("Your Account balance is %.2f%n",bank.accountBalance);
+		 
+			    }
+				break;
+				
+				case 2:
+				{
+					System.out.print("Enter amount to deposit: ");
+					int amount =input.nextInt();
+					System.out.printf("Account name is %s%n",bank.accountName);
+		 
+		            System.out.printf("Account Number is %s%n",bank.accountNumber);
+		 
+		 
+		            System.out.printf("Account Balance is %.2f%n",bank.accountBalance);
+					
+					bank.deposit(amount);
+					System.out.printf("Your account Balance is %.2f%n",bank.accountBalance);
+		 
+				}
+				break;
+				default:
+				System.out.println("Invalid Input");
+			 
+		    }
+		 
+		
+	    }
+	public double withdrawal(int amount){
+		 accountBalance -= amount;
+		 
+		 return accountBalance;
+	}
+	 
+	 public double deposit(int amount){
+		 accountBalance += amount;
+		 
+		 return accountBalance;
+
+		 
+	}
+	
+}
